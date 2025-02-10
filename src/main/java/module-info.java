5ebd788650c508a49d4d36bee4ae7fd4c0db433c
Bufigol <f.vasquez.tort@gmail.com>
2025-02-10 14:09:29 -0300

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.bufigol.nftsespaciomanchanodocentral {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.bufigol.nftsespaciomanchanodocentral to javafx.fxml;
+    exports com.bufigol.nftsespaciomanchanodocentral;
+}
